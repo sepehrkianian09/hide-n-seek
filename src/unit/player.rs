2fe327ab662aaae8937098630764108e42cfad53
sepehrkianian09 <sepehr.kianian.r@gmail.com>
2025-05-08 14:46:29 +0000

    refactor(player): separate builder

--- src/unit/player.rs
+++ src/unit/player.rs
@@ -7,6 +7,9 @@ use serde::{Deserialize, Serialize};
 
 use crate::{point::Point2d, traits::Position};
 
+pub mod builder;
+pub use builder::PlayerBuilder;
+
 #[derive(Debug, Default, Serialize, Deserialize, PartialEq)]
 pub struct Player {
     position: Point2d<f64>,
@@ -83,54 +86,6 @@ impl Position<f64> for Player {
 
     fn set_position(&mut self, position: Point2d<f64>) {
         self.position = position;
-    }
-}
-
-pub struct PlayerBuilder {
-    position: Point2d<f64>,
-    direction: Point2d<f64>,
-    speed: f64,
-    health: u8,
-}
-
-#[allow(clippy::new_without_default)]
-impl PlayerBuilder {
-    pub fn new() -> Self {
-        Self {
-            position: Point2d::new(1.0, 1.0),
-            direction: Point2d::new(1.0, 0.0),
-            speed: 0.0,
-            health: 10,
-        }
-    }
-
-    pub fn position(mut self, x: f64, y: f64) -> Self {
-        self.position = Point2d::new(x, y);
-        self
-    }
-
-    pub fn direction(mut self, x: f64, y: f64) -> Self {
-        self.direction = Point2d::new(x, y);
-        self
-    }
-
-    pub fn speed(mut self, speed: f64) -> Self {
-        self.speed = speed;
-        self
-    }
-
-    pub fn health(mut self, health: u8) -> Self {
-        self.health = health;
-        self
-    }
-
-    pub fn build(self) -> Player {
-        Player {
-            position: self.position,
-            direction: self.direction,
-            speed: self.speed,
-            health: self.health,
-        }
     }
 }
 
---
+++ src/unit/player/builder.rs
@@ -0,0 +1,51 @@
+use crate::point::Point2d;
+
+use super::Player;
+
+pub struct PlayerBuilder {
+    position: Point2d<f64>,
+    direction: Point2d<f64>,
+    speed: f64,
+    health: u8,
+}
+
+#[allow(clippy::new_without_default)]
+impl PlayerBuilder {
+    pub fn new() -> Self {
+        Self {
+            position: Point2d::new(1.0, 1.0),
+            direction: Point2d::new(1.0, 0.0),
+            speed: 0.0,
+            health: 10,
+        }
+    }
+
+    pub fn position(mut self, x: f64, y: f64) -> Self {
+        self.position = Point2d::new(x, y);
+        self
+    }
+
+    pub fn direction(mut self, x: f64, y: f64) -> Self {
+        self.direction = Point2d::new(x, y);
+        self
+    }
+
+    pub fn speed(mut self, speed: f64) -> Self {
+        self.speed = speed;
+        self
+    }
+
+    pub fn health(mut self, health: u8) -> Self {
+        self.health = health;
+        self
+    }
+
+    pub fn build(self) -> Player {
+        Player {
+            position: self.position,
+            direction: self.direction,
+            speed: self.speed,
+            health: self.health,
+        }
+    }
+}
