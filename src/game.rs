132ffbeb94ca2577d42a33d55620de80be77fa94
sepehrkianian09 <sepehr.kianian.r@gmail.com>
2025-05-09 12:18:04 +0000

    refactor(game): move update function of Collectible

--- src/game.rs
+++ src/game.rs
@@ -10,6 +10,7 @@ use std::{
 
 use builder::GameBuilder;
 use derivative::Derivative;
+use num::traits::NumAssign;
 use rand::{rngs::ThreadRng, Rng, RngCore};
 
 use serde::{Deserialize, Serialize};
@@ -23,8 +24,8 @@ use crate::{
     unit::{Collectible, Enemy, Player, PlayerBuilder, Wall},
 };
 
-fn rng_new() -> Box<dyn RngCore> {
-    Box::new(rand::thread_rng())
+fn rng_new() -> RefCell<Box<dyn RngCore>> {
+    RefCell::new(Box::new(rand::thread_rng()))
 }
 
 #[derive(Derivative, Serialize, Deserialize)]
@@ -38,13 +39,13 @@ pub struct Game {
     enemies: Vec<Enemy>,
     n_random_walls: u16,
     walls: Vec<Wall>,
-    collectible: Collectible,
+    collectible: RefCell<Collectible>,
     player: RefCell<Player>,
     #[serde(skip, default = "crate::ui::UI::new")]
     ui: UI,
     #[serde(skip, default = "rng_new")]
     #[derivative(Debug = "ignore")]
-    rng: Box<dyn RngCore>,
+    rng: RefCell<Box<dyn RngCore>>,
     update_interval_millis: Duration,
     hud: Hud,
 }
@@ -53,7 +54,6 @@ impl PartialEq for Game {
     fn eq(&self, other: &Self) -> bool {
         self.height == other.height
             && self.width == other.width
-            && self.score == other.score
             && self.enemies == other.enemies
             && self.n_random_walls == other.n_random_walls
             && self.walls == other.walls
@@ -88,14 +88,14 @@ impl Game {
         // add random walls
         for _ in 0..self.n_random_walls {
             let mut wall = Wall::default();
-            wall.set_rand_position(&mut self.rng, 1..self.width - 1, 1..self.height - 1);
+            wall.set_rand_position(&mut self.rng.borrow_mut(), 1..self.width - 1, 1..self.height - 1);
             self.walls.push(wall);
         }
 
         // randomize enemy positions
         self.enemies.iter_mut().for_each(|enemy| {
             enemy.set_rand_position(
-                &mut self.rng,
+                &mut self.rng.borrow_mut(),
                 1.0..(self.width - 1).into(),
                 1.0..(self.height - 1).into(),
             );
@@ -105,10 +105,10 @@ impl Game {
         while self
             .walls
             .iter()
-            .any(|wall| wall.position() == self.collectible.position())
+            .any(|wall| wall.position() == self.collectible.borrow().position())
         {
-            self.collectible.set_rand_position(
-                &mut self.rng,
+            self.collectible.borrow_mut().set_rand_position(
+                &mut self.rng.borrow_mut(),
                 1..self.width - 1,
                 1..self.height - 1,
             );
@@ -119,26 +119,20 @@ impl Game {
         self.walls.iter().any(|wall| wall.position() == position)
     }
 
+    pub fn player_position(&self) -> Point2d<f64> {
+        self.player.borrow().position()
+    }
+
+    pub fn randomize_position_u16(&self, a_position: &mut dyn Position<u16>) {
+        a_position.set_rand_position(&mut self.rng.borrow_mut(), 1..self.width - 1, 1..self.height - 1);
+    }
+
     fn update(&mut self, since_last_time: Duration) {
         self.player.borrow_mut().update(self, &since_last_time);
 
-        // increase score if player collides with collectible
-        if self.player.borrow().position().round().to_u16() == self.collectible.position() {
-            self.score += 1;
-            // move collectible to a new random position
-            self.collectible.set_rand_position(
-                &mut self.rng,
-                1..self.width - 1,
-                1..self.height - 1,
-            );
-            while self.do_walls_collide(self.collectible.position()) {
-                self.collectible.set_rand_position(
-                    &mut self.rng,
-                    1..self.width - 1,
-                    1..self.height - 1,
-                );
-            }
-        }
+        self.collectible
+            .borrow_mut()
+            .update(&self, &since_last_time);
 
         // move enemies
         self.enemies.iter_mut().for_each(|enemy: &mut Enemy| {
@@ -163,7 +157,7 @@ impl Game {
         self.enemies
             .iter()
             .for_each(|enemy| enemy.draw(&mut buffer));
-        self.collectible.draw(&mut buffer);
+        self.collectible.borrow().draw(&mut buffer);
         self.hud.draw(&mut buffer);
         self.stdout
             .write_all(&buffer)
--- src/unit/collectible.rs
+++ src/unit/collectible.rs
@@ -1,10 +1,13 @@
+use std::time::Duration;
+
 use serde::{Deserialize, Serialize};
 
-use crate::{point::Point2d, traits::Position};
+use crate::{game::Game, point::Point2d, traits::Position};
 
 #[derive(Debug, Default, Serialize, Deserialize, PartialEq)]
 pub struct Collectible {
     position: Point2d<u16>,
+    score: u32,
 }
 
 impl Position<u16> for Collectible {
@@ -14,5 +17,21 @@ impl Position<u16> for Collectible {
 
     fn set_position(&mut self, position: Point2d<u16>) {
         self.position = position;
+    }
+}
+
+impl Collectible {
+    pub fn update(&mut self, game: &Game, since_last_time: &Duration) {
+        let _ = since_last_time;
+        // increase score if player collides with collectible
+        if game.player_position().round().to_u16() == self.position() {
+            self.score += 1;
+            // move collectible to a new random position
+            game.randomize_position_u16(self);
+
+            while game.do_walls_collide(self.position()) {
+                game.randomize_position_u16(self);
+            }
+        }
     }
 }
